25fd27df8caeeff534a58aa46e07c4d9aba0672c
malikaisaeva <121940266+malikaisaeva@users.noreply.github.com>
2023-01-28 07:13:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let age = prompt('How old are you?')
+let first = 0-6
+let second = 7-14
+let third = 15-17
+let fourth = 18-21
+
+if(first >=0 && first <=6) {
+    console.log("малыщ");
+} else {
+    console.log("назад бро");
+}
+
+if (second >=7 &&second <=14) {
+    console.log("ребенок");
+} else {
+    console.log("подрости немного");
+}
+
+if (third >=15 && third <=17) {
+    console.log("подросток");
+} else {
+    console.log("до 18 нельзя");
+}
+
+if (fourth >=18 && fourth <=21) {
+    console.log("совершеннолетний");
+} else {
+    console.log("иди домой");
+}
+
